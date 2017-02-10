2b4e3cfcfc5ba210c2c544575631f59dd96735cf
Chris Ying <chrsyng@gmail.com>
2017-02-10 18:49:56 -0500

    tracker with sample output

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,9 +1,9 @@
 # Parameters used in this project all in one place
 
-VOT_DIR = '/Users/cying/space/research/vot'
-PROCESSED_DIR = '/Users/cying/space/research/vot/processed'
-#VOT_DIR = '/home/cying/vot2016'
-#PROCESSED_DIR = '/home/cying/processed_vot'
+#VOT_DIR = '/Users/cying/space/research/vot'
+#PROCESSED_DIR = '/Users/cying/space/research/vot/processed'
+VOT_DIR = '/home/cying/vot2016'
+PROCESSED_DIR = '/home/cying/processed_vot'
 
 KEY_FRAME_GAP = 10
 MAX_FRAME_GAP = 100
--- train_vgg_tracker.py
+++ train_vgg_tracker.py
@@ -16,7 +16,7 @@ from CONSTANTS import *
 #
 #batch1 = img1.reshape((1, 224, 224, 3))
 key = np.array(Image.open(os.path.join(PROCESSED_DIR, 'fish1', 'key-00000001', 'key-00000001.png'))).reshape([1, KEY_FRAME_SIZE, KEY_FRAME_SIZE, 3])
-search = np.array(Image.open(os.path.join(PROCESSED_DIR, 'fish1', 'key-00000001', 'search-00000002.png'))).reshape([1, SEARCH_FRAME_SIZE, SEARCH_FRAME_SIZE, 3])
+search = np.array(Image.open(os.path.join(PROCESSED_DIR, 'fish1', 'key-00000001', 'search-00000060.png'))).reshape([1, SEARCH_FRAME_SIZE, SEARCH_FRAME_SIZE, 3])
 
 with tf.device('/cpu:0'):
     sess = tf.Session()
@@ -28,7 +28,7 @@ with tf.device('/cpu:0'):
     search_image = tf.placeholder(tf.float32, [None, SEARCH_FRAME_SIZE, SEARCH_FRAME_SIZE, 3])
 
     vgg = vgg19.Vgg19('./vgg19.npy')
-    vgg.build(key_image, search_img)
+    vgg.build(key_image, search_image)
 
     # print number of variables used: 143667240 variables, i.e. ideal size = 548MB
     print vgg.get_var_count()
@@ -37,7 +37,11 @@ with tf.device('/cpu:0'):
 
     # test classification
     corr_map = sess.run(vgg.cross_corr5, feed_dict={key_image: key, search_image: search})
-
+    corr_map = corr_map.reshape((corr_map.shape[1], corr_map.shape[2]))
+    corr_map = (corr_map - np.min(corr_map))
+    corr_map = corr_map / (np.max(corr_map) + 0.0001)
+    im = Image.fromarray(np.uint8(corr_map * 255))
+    im.save('corr_map.png')
     #utils.print_prob(prob[0], './synset.txt')
 
     ## simple 1-step training
