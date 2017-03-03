8640976959b726af1570b1f502d06e6253a9a903
Chris Ying <chrsyng@gmail.com>
2017-03-03 01:45:15 -0500

    better visual

--- train_vgg_tracker.py
+++ train_vgg_tracker.py
@@ -109,7 +109,7 @@ def visualize_corr_maps(sess, vgg, name, k, s, g, key_img, search_img, ground_im
     red[0,:,:] = 255
     search_img = search_img.reshape((SEARCH_FRAME_SIZE, SEARCH_FRAME_SIZE, 3))
     combined_search = np.where(ground_img==-1, search_img, red)
-    new_im.paste(Image.fromarray(combined_search, (SEARCH_FRAME_SIZE+2*PAD + PAD, PAD)))
+    new_im.paste(Image.fromarray(combined_search), (SEARCH_FRAME_SIZE+2*PAD + PAD, PAD))
 
     for i, ci in enumerate([c1,c2,c3,c4,c5]):
         new_im.paste(ci, ((i+2) * (SEARCH_FRAME_SIZE+2*PAD) + PAD, PAD))
