9488a36e2846b679f7325e6259df8f95c5cc9813
Chris Ying <chrsyng@gmail.com>
2017-03-04 13:00:48 -0500

    testing tiger121

--- train_vgg_tracker.py
+++ train_vgg_tracker.py
@@ -138,6 +138,7 @@ def main():
         sess.run(tf.global_variables_initializer())
 
         key, search, ground = load_batch('tiger', 'key-00000121')
+        print key.shape, search.shape, ground.shape
         visualize_corr_maps(sess, vgg, 'tiger-00000121.png', key_image, search_image, ground_truth,
                             key[30:31,:,:,:], search[30:31,:,:,:], ground[30:31,:,:,:])
 
