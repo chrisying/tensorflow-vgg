10ecf3e980c67293e808249f85f1acf63bf52393
Chris Ying <chrsyng@gmail.com>
2017-03-04 13:04:39 -0500

    testing tiger121

--- train_vgg_tracker.py
+++ train_vgg_tracker.py
@@ -109,6 +109,8 @@ def visualize_corr_maps(sess, vgg, name, k, s, g, key_img, search_img, ground_im
     red = np.zeros((SEARCH_FRAME_SIZE, SEARCH_FRAME_SIZE, 3), dtype='uint8')
     red[:,:,2] = 255
     combined_search = np.where(ground_img<0, search_img, red).reshape((SEARCH_FRAME_SIZE, SEARCH_FRAME_SIZE, 3))
+    print search_img.dtype
+    print np.max(search_img)
     print combined_search.dtype
     print combined_search.shape
     new_im.paste(Image.fromarray(combined_search), (SEARCH_FRAME_SIZE+2*PAD + PAD, PAD))
