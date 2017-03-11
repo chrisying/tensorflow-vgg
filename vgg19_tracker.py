cb4e14867dd1c4f484b47fea3f00dcca2a2a978a
Chris Ying <chrsyng@gmail.com>
2017-03-11 13:15:52 -0500

    trying only xcorr5 loss

--- vgg19_tracker.py
+++ vgg19_tracker.py
@@ -144,7 +144,8 @@ class Vgg19:
                                   self.conf5 * self.rcorr5) /
                                   (self.conf1 + self.conf2 + self.conf3 + self.conf4 + self.conf5 + 0.0001))
 
-        self.raw_loss =  tf.reduce_mean(tf.log(1.0 + tf.exp(-ground_truth * self.raw_prediction)))
+        #self.raw_loss =  tf.reduce_mean(tf.log(1.0 + tf.exp(-ground_truth * self.raw_prediction)))
+        self.raw_loss =  tf.reduce_mean(tf.log(1.0 + tf.exp(-ground_truth * self.rcorr5)))
         # TODO: add computation cost
         self.gated_loss = self.weighted_logistic_loss(ground_truth, self.gated_prediction)
 
