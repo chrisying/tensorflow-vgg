662086b8cf141928d864d6bbee13097b978aaaaf
Chris Ying <chrsyng@gmail.com>
2017-03-23 18:18:15 -0400

    .

--- train_vgg_tracker.py
+++ train_vgg_tracker.py
@@ -194,8 +194,8 @@ def main():
 
         diagnostic_corr_maps(sess, vgg, 'initial.png')
 
-        #valid_loss, iou1, iou5, iou20 = run_validation(sess, vgg)
-        #print '[VALID] Initial validation loss: %.5f, IOU@1: %.5f, IOU@5: %.5f, IOU@20: %.5f' % (valid_loss, iou1, iou5, iou20)
+        valid_loss, iou1, iou5, iou20 = run_validation(sess, vgg)
+        print '[VALID] Initial validation loss: %.5f, IOU@1: %.5f, IOU@5: %.5f, IOU@20: %.5f' % (valid_loss, iou1, iou5, iou20)
 
         # TODO: use QueueRunner to optimize file loading on CPU
         print 'Starting training'
@@ -225,8 +225,9 @@ def main():
             epoch_loss = epoch_loss_sum / num_samples
             print '[TRAIN] Samples considered: %d' % num_samples
             print '[TRAIN] Epoch %d loss: %.5f' % (epoch+1, epoch_loss)
-            #valid_loss, iou1, iou5, iou20 = run_validation(sess, vgg)
-            #print '[VALID] Epoch %d, validation loss: %.5f, IOU@1: %.5f, IOU@5: %.5f, IOU@20: %.5f' % (epoch+1, valid_loss, iou1, iou5, iou20)
+
+            valid_loss, iou1, iou5, iou20 = run_validation(sess, vgg)
+            print '[VALID] Epoch %d, validation loss: %.5f, IOU@1: %.5f, IOU@5: %.5f, IOU@20: %.5f' % (epoch+1, valid_loss, iou1, iou5, iou20)
 
             # checkpointing
             diagnostic_corr_maps(sess, vgg, 'epoch_%s.png' % str(epoch+1).zfill(3))
