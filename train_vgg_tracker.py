fixed weighted softmax, debugging NAN loss

train_vgg_tracker.py
@@ -105,8 +105,8 @@ def convert_corr_map(corr_map):
 
 def visualize_corr_maps(sess, vgg, name, key_img, search_img, key_bb, search_bb):
     # Expects key_img, search_img, ground_img to be batch size 1
-    [gts, weight, cm1, cm2, cm3, cm4, cm5, con1, con2, con3, con4, con5] = sess.run(
-            [vgg.ground_truth, vgg.weight, vgg.rcorr1, vgg.rcorr2, vgg.rcorr3, vgg.rcorr4, vgg.rcorr5,
+    [cm1, cm2, cm3, cm4, cm5, con1, con2, con3, con4, con5] = sess.run(
+            [vgg.rcorr1, vgg.rcorr2, vgg.rcorr3, vgg.rcorr4, vgg.rcorr5,
              vgg.conf1, vgg.conf2, vgg.conf3, vgg.conf4, vgg.conf5],
             feed_dict={
                 vgg.key_img: key_img,
@@ -114,10 +114,6 @@ def visualize_corr_maps(sess, vgg, name, key_img, search_img, key_bb, search_bb)
                 vgg.key_bb: key_bb,
                 vgg.search_bb: search_bb})
 
-    print weight
-    print weight.shape
-    print np.max(weight)
-    print np.sum(weight)
     c1 = convert_corr_map(cm1)
     c2 = convert_corr_map(cm2)
     c3 = convert_corr_map(cm3)
@@ -199,7 +195,6 @@ def main():
 
         diagnostic_corr_maps(sess, vgg, 'initial.png')
 
-        '''
         valid_loss, iou1, iou5, iou20 = run_validation(sess, vgg)
         print '[VALID] Initial validation loss: %.5f, IOU@1: %.5f, IOU@5: %.5f, IOU@20: %.5f' % (valid_loss, iou1, iou5, iou20)
 
@@ -225,6 +220,7 @@ def main():
                                 vgg.key_bb: key_bb,
                                 vgg.search_bb: search_bb})
 
+                    print '[TRAIN] Batch loss %s %s: %.5f' % (train_cat, key_name, loss)
                     epoch_loss_sum += BATCH_SIZE * loss
                     num_samples += BATCH_SIZE
 
@@ -245,7 +241,6 @@ def main():
         # save model
         #vgg.save_npy(sess, './trained_model_%s.npy' % str(int(time.time())))
         sess.close()
-        '''
 
 if __name__ == '__main__':
     main()