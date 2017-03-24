ee3ac3211b657a49423179b2b736ccdbeb3476a1
Chris Ying <chrsyng@gmail.com>
2017-03-24 00:25:43 -0400

    .

--- vgg19_tracker.py
+++ vgg19_tracker.py
@@ -266,14 +266,17 @@ class Vgg19:
         flattened_shape = [-1, shape[1] * shape[2] * shape[3]]
 
         normalized_ground_truth = ground_truth / tf.reduce_sum(ground_truth, axis=[1,2,3], keep_dims=True)
-        normalized_ground_truth = tf.reshape(normalized_ground_truth, flattened_shape)
 
-        scale = tf.constant((SEARCH_FRAME_SIZE ** 2), dtype=tf.float32) / tf.reduce_sum(ground_truth)
-        weight = tf.where(normalized_ground_truth > 0, tf.ones_like(normalized_ground_truth) * scale, tf.ones_like(normalized_ground_truth))
+        num_positive = tf.reduce_sum(tf.cast(ground_truth > 0.1, tf.float32)
+        scale = tf.reduce_sum(tf.ones_like(ground_truth, dtype=tf.float32)) / num_positive
+        weight = tf.where(ground_truth > 0.1, tf.ones_like(ground_truth) * scale, tf.ones_like(ground_truth))
+
+        reshaped_ground_truth = tf.reshape(normalized_ground_truth, flattened_shape)
         reshaped_prediction = tf.reshape(prediction, flattened_shape)
-        weighted_logits = reshaped_prediction * weight    # TODO: should we normalized prediction?
+        reshaped_weights = tf.reshape(weight, flattened_shape)
 
-        softmax_loss = tf.nn.softmax_cross_entropy_with_logits(logits=weighted_logits, labels=normalized_ground_truth)
+        weighted_logits = reshaped_prediction * reshaped_weights
+        softmax_loss = tf.nn.softmax_cross_entropy_with_logits(logits=weighted_logits, labels=reshaped_ground_truth)
         loss = tf.reduce_mean(softmax_loss)
 
         return loss, weight
