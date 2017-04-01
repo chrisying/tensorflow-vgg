d2e59d36f7d9a69543d6151028f3e151e437d05b
Chris Ying <chrsyng@gmail.com>
2017-04-01 15:50:09 -0400

    randomize train order

--- train_vgg_tracker.py
+++ train_vgg_tracker.py
@@ -188,14 +188,14 @@ def main():
         # print number of variables used: 143667240 variables, i.e. ideal size = 548MB
         print vgg.get_var_count()
 
-        global_step = tf.Variable(0, trainable=False)
-        starter_learning_rate = 1e-5
-        decay_steps = 10000
-        decay_rate = 0.95
+        #global_step = tf.Variable(0, trainable=False)
+        #starter_learning_rate = 1e-5
+        #decay_steps = 10000
+        #decay_rate = 0.95
 
-        learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, decay_steps, decay_rate, staircase=True)
-        train_finetune = tf.train.AdamOptimizer(learning_rate).minimize(vgg.raw_loss, var_list=vgg.cnn_var_list, global_step=global_step)
-        #train_finetune = tf.train.AdamOptimizer(1e-5).minimize(vgg.raw_loss, var_list=vgg.cnn_var_list)
+        #learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, decay_steps, decay_rate, staircase=True)
+        #train_finetune = tf.train.AdamOptimizer(learning_rate).minimize(vgg.raw_loss, var_list=vgg.cnn_var_list, global_step=global_step)
+        train_finetune = tf.train.AdamOptimizer(1e-5).minimize(vgg.raw_loss, var_list=vgg.cnn_var_list)
         #train_finetune = tf.train.MomentumOptimizer(1e-5, 0.9999).minimize(vgg.raw_loss, var_list=vgg.cnn_var_list, global_step=global_step)
         #train_gate = tf.train.AdamOptimizer(1e-5).minimize(vgg.gated_loss, var_list=vgg.gate_var_list)
         sess.run(tf.global_variables_initializer())
@@ -219,33 +219,38 @@ def main():
             iou5_sum = 0.0
             iou50_sum = 0.0
             num_samples = 0
-            for train_cat in TRAIN_CATS:
-                cat_dir = os.path.join(PROCESSED_DIR, train_cat)
-                key_names = os.listdir(cat_dir)
-                for key_name in key_names:
+
+            with open(os.path.join(PROCESSED_DIR, 'train.txt')) as f:
+                train_samples = f.readlines()
+            order = np.random.permutation(len(train_samples))
+
+            for train_idx in train_samples:
+                #cat_dir = os.path.join(PROCESSED_DIR, train_cat)
+                #key_names = os.listdir(cat_dir)
+                #for key_name in key_names:
                     # ordering shouldn't matter
-                    key, search, key_bb, search_bb = load_batch(train_cat, key_name)
-                    if key is None:
-                        continue
+                train_cat, key_name = train_samples[train_idx].split('/')
 
-                    #_, loss = sess.run([train_finetune, vgg.raw_loss],
-                    _, loss, iou1, iou5, iou50 = sess.run([train_finetune, vgg.raw_loss, vgg.IOU_at_1, vgg.IOU_at_5, vgg.IOU_full],
-                            feed_dict={
-                                vgg.key_img: key,
-                                vgg.search_img: search,
-                                vgg.key_bb: key_bb,
-                                vgg.search_bb: search_bb})
+                key, search, key_bb, search_bb = load_batch(train_cat, key_name)
+                if key is None:
+                    continue
 
-                    #print '[TRAIN] Batch loss %s %s: %.5f' % (train_cat, key_name, loss)
-                    epoch_loss_sum += BATCH_SIZE * loss
-                    iou1_sum += BATCH_SIZE * iou1
-                    iou5_sum += BATCH_SIZE * iou5
-                    iou50_sum += BATCH_SIZE * iou50
-                    num_samples += BATCH_SIZE
+                #_, loss = sess.run([train_finetune, vgg.raw_loss],
+                _, loss, iou1, iou5, iou50 = sess.run([train_finetune, vgg.raw_loss, vgg.IOU_at_1, vgg.IOU_at_5, vgg.IOU_full],
+                        feed_dict={
+                            vgg.key_img: key,
+                            vgg.search_img: search,
+                            vgg.key_bb: key_bb,
+                            vgg.search_bb: search_bb})
+
+                #print '[TRAIN] Batch loss %s %s: %.5f' % (train_cat, key_name, loss)
+                epoch_loss_sum += BATCH_SIZE * loss
+                iou1_sum += BATCH_SIZE * iou1
+                iou5_sum += BATCH_SIZE * iou5
+                iou50_sum += BATCH_SIZE * iou50
+                num_samples += BATCH_SIZE
 
             epoch_loss = epoch_loss_sum / num_samples
-            #print '[TRAIN] Samples considered: %d' % num_samples
-            #print '[TRAIN] Epoch %d loss: %.5f' % (epoch+1, epoch_loss)
             print '[TRAIN] Epoch %d, loss: %.5f, IOU@1: %.5f, IOU@5: %.5f, IOU@50: %.5f' % (epoch+1, epoch_loss, iou1_sum / num_samples, iou5_sum / num_samples, iou50_sum / num_samples)
 
             valid_loss, iou1, iou5, iou50 = run_validation(sess, vgg)
